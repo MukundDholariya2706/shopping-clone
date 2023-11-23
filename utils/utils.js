eee0680663e01ff7f0ab649892c27ff7a1611d25
gaurangpltridhya <gaurangpltridhyatech@gmail.com>
2023-11-23 14:35:55 +0530

    if file exists in system than delete the file

--- utils/utils.js
+++ utils/utils.js
@@ -3,8 +3,12 @@ const fs = require("fs");
 
 const unlinkImage = (fileName) => {
   try {
+    const filePath = path.join(__dirname, "..");
     const file = path.basename(fileName);
-    fs.unlinkSync(`public/${file}`);
+
+    if (fs.existsSync(filePath + "/public/" + file)) {
+      fs.unlinkSync(`public/${file}`);
+    }
   } catch (error) {
     throw error;
   }
